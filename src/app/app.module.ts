3a9dbfda4562459abe28f6bfff561bbc76e4a578
João Victor <joaovictormendonca2406@gmail.com>
2023-10-16 02:48:10 -0300

    Update .gitignore, README.md, and 56 more files...

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterLoginComponent } from './pages/login/register-login/register-login.component';
+import { RedefinePasswordComponent } from './pages/login/redefine-password/redefine-password.component';
+import { DeleteComponent } from './modals/delete/delete.component';
+import { SaveComponent } from './modals/save/save.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterLoginComponent,
+    RedefinePasswordComponent,
+    DeleteComponent,
+    SaveComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/login/register-login/register-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-login',
+  templateUrl: './register-login.component.html',
+  styleUrls: ['./register-login.component.scss']
+})
+export class RegisterLoginComponent {
+
+}
